e16fd09036d8ca2b111d945e1c879dfaa5ee9205
test-dan-run <daniel.leongsj@gmail.com>
2022-02-19 11:05:15 +0800

    add structured configs

--- clearml_finetune.py
+++ clearml_finetune.py
@@ -2,7 +2,7 @@
 from clearml import Task, Dataset
 from config.config import ClearMLConfig as c_cfg
 
-Task.add_requirements('librosa')
+Task.add_requirements('librosa', 'omegaconf')
 task = Task.init(
     project_name=c_cfg.project_name, 
     task_name=c_cfg.task_name, 
@@ -17,15 +17,21 @@ from dataloader import DataCollatorCTCWithPadding
 from dataset_utils import generate_vocab_json, prepare_dataset
 
 import os
+from omegaconf import OmegaConf
 from transformers.integrations import TensorBoardCallback
 from datasets import load_dataset, load_metric, Audio
-from config.config import DatasetConfig as ds_cfg, \
-     ModelConfig as m_cfg, TrainingConfig as t_cfg
+from config.config import DatasetConfig, ModelConfig, TrainingArgumentsConfig
 from transformers import Trainer, TrainingArguments, \
      Wav2Vec2CTCTokenizer, Wav2Vec2FeatureExtractor, \
      Wav2Vec2Processor, Wav2Vec2ForCTC
 
 def main():
+
+    # import confs
+    ds_cfg = OmegaConf.structured(DatasetConfig)
+    m_cfg = OmegaConf.structured(ModelConfig)
+    t_cfg = OmegaConf.structured(TrainingArgumentsConfig)
+
     print('Downloading Dataset from S3 via ClearML...')
     dataset = Dataset.get(dataset_project=ds_cfg.dataset_project, dataset_name=ds_cfg.dataset_name)
     dataset_path = dataset.get_local_copy()
@@ -78,44 +84,15 @@ def main():
     wer_metric = load_metric('wer')
 
     print('Datasets prepared. Initiating model...')
-    model = Wav2Vec2ForCTC.from_pretrained(
-        m_cfg.pretrained_model_name, 
-        attention_dropout = m_cfg.attention_dropout,
-        hidden_dropout = m_cfg.hidden_dropout,
-        feat_proj_dropout = m_cfg.feat_proj_dropout,
-        mask_time_prob = m_cfg.mask_time_prob,
-        layerdrop = m_cfg.layerdrop,
-        ctc_loss_reduction = m_cfg.ctc_loss_reduction, 
-        ctc_zero_infinity = m_cfg.ctc_zero_infinity,
-        pad_token_id = processor.tokenizer.pad_token_id,
-        vocab_size = len(processor.tokenizer),
-    )
+    m_cfg['pad_token_id'] = processor.tokenizer.pad_token_id
+    m_cfg['vocab_size'] = len(processor.tokenizer)
+
+    model = Wav2Vec2ForCTC.from_pretrained(**m_cfg)
 
     model.freeze_feature_extractor()
     print('Model initiated. Preparing Trainer...')
 
-    training_args = TrainingArguments(
-        output_dir = t_cfg.output_dir,
-        group_by_length = t_cfg.group_by_length,
-        per_device_train_batch_size = t_cfg.per_device_train_batch_size,
-        per_device_eval_batch_size = t_cfg.per_device_eval_batch_size,
-        gradient_accumulation_steps = t_cfg.gradient_accumulation_steps,
-        evaluation_strategy = t_cfg.evaluation_strategy,
-        num_train_epochs = t_cfg.num_train_epochs,
-        gradient_checkpointing = t_cfg.gradient_checkpointing,
-        fp16 = t_cfg.fp16,
-        save_steps = t_cfg.save_steps,
-        eval_steps = t_cfg.eval_steps,
-        logging_steps = t_cfg.logging_steps,
-        learning_rate = t_cfg.learning_rate,
-        warmup_steps = t_cfg.warmup_steps,
-        load_best_model_at_end = t_cfg.load_best_model_at_end,
-        metric_for_best_model = t_cfg.metric_for_best_model,
-        greater_is_better = t_cfg.greater_is_better,
-        save_total_limit = t_cfg.save_total_limit,
-        dataloader_num_workers = t_cfg.dataloader_num_workers,
-        push_to_hub = t_cfg.push_to_hub,
-    )
+    training_args = TrainingArguments(**t_cfg)
 
     trainer = Trainer(
         model = model,
--- config/config.py
+++ config/config.py
@@ -1,52 +1,63 @@
+from dataclasses import dataclass
+from omegaconf import MISSING
+from typing import Tuple
+
+@dataclass
 class ClearMLConfig:
-    project_name = 'e2e-selfsupervised-asr/xlsr-finetune'
-    task_name = 'cv_magichub'
-    output_uri = 's3://experiment-logging/storage'
-    task_type = 'training'
-    tags = ['Wav2Vec2', 'Not Fairseq']
+    project_name: str = 'e2e-selfsupervised-asr/xlsr-finetune'
+    task_name: str = 'cv_magichub'
+    output_uri: str = 's3://experiment-logging/storage'
+    task_type:str  = 'training'
+    tags: Tuple[str] = ('Wav2Vec2', 'Not Fairseq')
 
-    base_docker = 'dleongsh/huggingface_asr:v4.16.2'
-    queue_name = 'compute'
+    base_docker: str = 'dleongsh/huggingface_asr:v4.16.2'
+    queue_name: str = 'compute'
 
+@dataclass
 class DatasetConfig:
+    dataset_project: str = 'indonesian_corpus'
+    dataset_name: str = 'cv_magichub'
 
-    dataset_project = 'indonesian_corpus'
-    dataset_name = 'cv_magichub'
+    feature_size : int = 1
+    sample_rate : int = 16000
+    padding_value : float = 0.0
+    do_normalize: bool = True
+    return_attention_mask: bool = True
 
-    feature_size = 1
-    sample_rate = 16000
-    padding_value = 0.0
-    do_normalize = True
-    return_attention_mask = True
-
+@dataclass
 class ModelConfig:
-    pretrained_model_name = "facebook/wav2vec2-xls-r-300m"
-    attention_dropout = 0.1
-    hidden_dropout = 0.1
-    feat_proj_dropout = 0.1
-    mask_time_prob = 0.05
-    layerdrop = 0.1
-    ctc_loss_reduction = "mean"
-    ctc_zero_infinity = True
-
-class TrainingConfig:
-    output_dir = 'output'
-    group_by_length=True
-    per_device_train_batch_size=16
-    per_device_eval_batch_size=16
-    gradient_accumulation_steps=2
-    evaluation_strategy="steps"
-    num_train_epochs=100
-    gradient_checkpointing=True
-    fp16=True
-    save_steps=500
-    eval_steps=500
-    logging_steps=100
-    learning_rate=3e-4
-    warmup_steps=1000
-    load_best_model_at_end = True
-    metric_for_best_model = 'wer'
-    greater_is_better = False
-    dataloader_num_workers = 8
-    save_total_limit=1
-    push_to_hub=False
+    pretrained_model_name: str = 'facebook/wav2vec2-xls-r-300m'
+    attention_dropout : float = 0.1
+    hidden_dropout : float = 0.1
+    feat_proj_dropout : float = 0.2
+    mask_time_prob : float = 0.05
+    layerdrop : float = 0.1
+    ctc_loss_reduction: str = 'mean'
+    ctc_zero_infinity: bool = True
+    pad_token_id: int = MISSING
+    vocab_size: int = MISSING
+    
+@dataclass
+class TrainingArgumentsConfig:
+    output_dir: str = 'output'
+    group_by_length = True
+    per_device_train_batch_size: int = 16
+    per_device_eval_batch_size: int = 16
+    gradient_accumulation_steps: int = 2
+    evaluation_strategy: str = 'steps'
+    num_train_epochs: int = 100
+    gradient_checkpointing: bool = False
+    fp16: bool = True
+    save_steps: int = 500
+    eval_steps: int = 500
+    logging_steps: int = 100
+    learning_rate: float = 5e-4
+    lr_scheduelr_type: str = 'linear'
+    warmup_steps: int = 1000
+    weight_decay: float = 0.005
+    load_best_model_at_end: bool = True
+    metric_for_best_model: str = 'wer'
+    greater_is_better: bool = False
+    dataloader_num_workers : int = 8
+    save_total_limit: int = 1
+    push_to_hub: bool = False
